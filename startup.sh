32ef5f599eb5c9b7a0fe50f5b2cc62c69f2b8a2b
Dieu Merci <dieumercin21@gmail.com>
2026-04-03 00:12:16 +0200

    fix startup script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
-echo "=== Running migrations ==="
-python manage.py migrate --run-syncdb
-echo "=== Migrations done ==="
-echo "=== Creating superuser ==="
-python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser already exists"
-echo "=== Starting server ==="
+echo "Running migrations"
+python manage.py migrate
+echo "Creating superuser"
+python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser exists"
+echo "Starting server"
 exec gunicorn --bind :8080 --workers 2 core.wsgi

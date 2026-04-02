5d0c2ada170c34fe69eb990a799310d832d79393
Dieu Merci <dieumercin21@gmail.com>
2026-04-02 23:58:47 +0200

    fix: switch to PostgreSQL database

--- core/settings.py
+++ core/settings.py
@@ -55,10 +55,9 @@ TEMPLATES = [{
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3')
+    )
 }
 
 REST_FRAMEWORK = {

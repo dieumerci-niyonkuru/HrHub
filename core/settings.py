8c605d76b9fb70bd78544b92ccc264ae6c8d8a8f
Dieu Merci <dieumercin21@gmail.com>
2026-04-02 23:15:46 +0200

    fix: use simpler whitenoise storage

--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'frontend/dist/assets'] if (BASE_DIR / 'frontend/dist/assets').exists() else []
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

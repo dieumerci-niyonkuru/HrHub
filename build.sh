9b3ea502d68f6d8ad0ab8d5a59ce887aa6d6be59
Dieu Merci <dieumercin21@gmail.com>
2026-04-02 23:14:54 +0200

    fix: clear static files before collecting

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py collectstatic --no-input --clear
 python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL || true

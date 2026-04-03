966480a9437169eca216449eae9c2cf4c1c38e7a
Dieu Merci <dieumercin21@gmail.com>
2026-04-03 10:48:38 +0200

    fix: create superuser using shell command

--- startup.sh
+++ startup.sh
@@ -3,6 +3,13 @@ set -e
 echo "Running migrations"
 python manage.py migrate
 echo "Creating superuser"
-python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser exists"
+python manage.py shell -c "
+from users.models import User
+if not User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
+    User.objects.create_superuser(email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD', first_name='Admin', last_name='User')
+    print('Superuser created!')
+else:
+    print('Superuser already exists')
+"
 echo "Starting server"
 exec gunicorn --bind :8080 --workers 2 core.wsgi
